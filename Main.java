84c6ca62fe15e403d051cb360103a792924d7c57
Wisansiiz <81199104+Wisansiiz@users.noreply.github.com>
2021-03-23 15:13:01 +0800

    更新代码
    为一些同学提供代码，记得点star。

---
+++ Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				Scanner reader =new Scanner(System.in);
+		        int a1,a2,b1,b2,c1,c2,d1,d2,a3,a4,a5,a6,b3,b4,b5,b6,c3,c4,c5,c6,d3,d4,d5,d6;
+		        double s1,s2,s3;
+		        
+		            a1 = reader.nextInt();
+		            a2 = reader.nextInt();
+		            b1 = reader.nextInt();
+		            b2 = reader.nextInt();
+		            c1 = reader.nextInt();
+		            c2 = reader.nextInt();
+		            d1 = reader.nextInt();
+		            d2 = reader.nextInt();
+		            a3 = reader.nextInt();
+		            a4 = reader.nextInt();
+		            b3 = reader.nextInt();
+		            b4 = reader.nextInt();
+		            c3 = reader.nextInt();
+		            c4 = reader.nextInt();
+		            d3 = reader.nextInt();
+		            d4 = reader.nextInt();
+		            a5 = reader.nextInt();
+		            a6 = reader.nextInt();
+		            b5 = reader.nextInt();
+		            b6 = reader.nextInt();
+		            c5 = reader.nextInt();
+		            c6 = reader.nextInt();
+		            d5 = reader.nextInt();
+		            d6 = reader.nextInt();
+		            
+		          s1=(b1-a1+c1-d1)*(a2-d2)*(0.5);
+		          s2=(b3-a3+c3-d3)*(a4-d4)*(0.5);
+		          s3=(b5-a5+c5-d5)*(a6-d6)*(0.5);
+		          System.out.printf("area = %.4f\n",s1);
+		          System.out.printf("area = %.4f\n",s2);
+		          System.out.printf("area = %.4f",s3);
+		          
+		            
+		        
+		            
+	}
+
+}
